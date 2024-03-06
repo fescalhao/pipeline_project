814075253bde1103516d4c365a90810693691d19
Felipe Alberto Silva Escalhão <fescalhao@Felipes-MacBook-Pro.local>
2024-03-05 21:02:06 -0300

    Fixing errors

--- airflow_project/dags/movies_project/spark_movies.py
+++ airflow_project/dags/movies_project/spark_movies.py
@@ -5,7 +5,7 @@ from airflow.models import Variable
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 
 from dag_utils.spark_app_template.builder import build_spark_app_template
-from movie_utils import get_spark_arguments
+from movies_project.movie_utils import get_spark_arguments
 
 with DAG(
         dag_id="spark_movies",
